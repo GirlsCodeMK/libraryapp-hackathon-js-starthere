5f8375db4d9a923745f650e6be122341ce881dd3
weresquirrel <vermokuss@gmail.com>
2019-01-19 16:36:38 +0000

    search feature added

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import loans from './loans';
 
 import { Book, User } from '../models';
 import catchAsync from '../lib/catchAsync';
+import Sequelize from 'sequelize';
 
 const router = Router();
 
@@ -19,6 +20,32 @@ router.get('/', catchAsync(async (req, res) => {
   const books_count = await Book.count();
   const users_count = await User.count();
   res.render('index', {books_count, users_count});
+}));
+
+router.post('/', catchAsync(async (req, res) => {
+  let books;
+  try {
+    const Op = Sequelize.Op;
+    books =
+      await Book.findAll({ where:
+        {
+          [Op.or]: [
+            {title:
+              {[Op.iLike]: '%'+req.body.q+'%'}
+            },
+            {author:
+              {[Op.iLike]: '%'+req.body.q+'%'}
+            }
+          ]
+        }
+
+
+        });
+  } catch(e) {
+    console.warn(e);
+    req.flash('alert', e.toString());
+  }
+  res.render('books/index', {books: books});
 }));
 
 export default router;

43305a222499e8b9baf448fdf80714c6e69e0500
Lisa Hughes <37778541+leese81@users.noreply.github.com>
2019-01-20 10:57:14 +0000

    Added role to the user and edit role functionality

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,17 +3,41 @@ import { User } from '../models';
 import catchAsync from '../lib/catchAsync';
 
 const router = Router();
+// Fields people are permitted to modify using the API.
+const permittedParams = ['email', 'role'];
 
 // Sign up form
 router.get('/new', (req, res) => {
   res.render('users/new', {user: null, error: null});
 });
 
+// View user record
+router.get('/:id', catchAsync(async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  res.render('users/edit', {user: user, error: null});
+}));
+
 // View all users
 router.get('/', catchAsync(async (req, res) => {
   const users = await User.findAll({});
 
   res.render('users/index', {users: users});
+}));
+
+// Update a user
+router.post('/:id', catchAsync(async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    permittedParams.forEach((field) => user[field] = req.body[field]);
+    user.save();
+    res.redirect('/users');
+  } catch(e) {
+    console.warn(e);
+    res.status(400).send(e.toString());
+  }
 }));
 
 // Create a user (sign up)

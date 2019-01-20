99ec97a75c60932d692cae14c4adc77fad96c22f
David Somers <126989+jalada@users.noreply.github.com>
2019-01-20 14:01:23 +0000

    Tweak whitespace

--- src/migrations/20190120114903-add-new-user-fields.js
+++ src/migrations/20190120114903-add-new-user-fields.js
@@ -2,30 +2,28 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return  queryInterface.addColumn('Users', 'name', {
+    return queryInterface.addColumn('Users', 'name', {
+      allowNull: true,
+      type: Sequelize.STRING
+    }).then(() => 
+      queryInterface.addColumn('Users', 'membershipDate', {
+        allowNull: true,
+        type: Sequelize.DATE
+      })
+    ).then(() =>
+      queryInterface.addColumn('Users', 'phone', {
         allowNull: true,
         type: Sequelize.STRING
-      }).then(() => 
-        queryInterface.addColumn('Users', 'membershipDate', {
-          allowNull: true,
-          type: Sequelize.DATE
-        })
-      ).then(() =>
-        queryInterface.addColumn('Users', 'phone', {
-          allowNull: true,
-          type: Sequelize.STRING
-        })
-      );
+      })
+    );
    },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Users', 'name' )
-      .then(
-        () => 
-        queryInterface.removeColumn('Users', 'membershipDate' )
-      ).then(
-        () => 
-        queryInterface.removeColumn('Users', 'phone' )
+    return queryInterface.removeColumn('Users', 'name')
+      .then(() => 
+        queryInterface.removeColumn('Users', 'membershipDate')
+      ).then(() => 
+        queryInterface.removeColumn('Users', 'phone')
       );
   }
 };

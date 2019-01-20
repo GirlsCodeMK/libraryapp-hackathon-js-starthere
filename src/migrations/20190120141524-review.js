851a785cfb09e300226dc64295e1cb4ea90883bd
rjp44 <rob@ipcortex.co.uk>
2019-01-20 14:29:09 +0000

    Migration implementation

--- src/migrations/20190120141524-review.js
+++ src/migrations/20190120141524-review.js
@@ -1,23 +1,52 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Reviews', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            BookId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Books',
+                    key: 'id'
+                }
+            },
+            UserId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Users',
+                    key: 'id'
+                }
+            },
+            Rating: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            Text: {
+                defaultValue: false,
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        (queryInterface, Sequelize) => {
+            return queryInterface.dropTable('Reviews');
+        }
+    }
 };
